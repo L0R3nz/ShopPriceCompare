6e18d185b684f2c5a267a1b5b56f32c36c98cd29
HALUPRL <mr.L0R3nz@gmail.com>
2020-08-09 17:32:09 +0200

    Modify main file and prepper core function for implementation

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import random
 
 from time import sleep
 from CeneoGrabber import getCeneoPrice
+from os import path, remove
 
 def create_database():
     
@@ -61,16 +62,47 @@ def create_report():
             item['ceneo_price']  = getCeneoPrice(item['ceneo_id']);
             report_list.append(item)
 
-            wait_time = random.randint(60, 5*60) # wait from 1 minute to 5 minutes to avoid captcha on ceneo
+            wait_time = random.randint(60, 5*60) # wait from 1 minute to 5 minutes to avoid captcha on ceneo.pl
             
             print("Wait {0:<3} sec | {1}".format(wait_time, item))
             save_file("Report.json", report_list)
             sleep(wait_time)
-            
     
 
+def analize_data(data):
+    return data           
 
-print("Create database")
-create_database()
-print("Get data from ceneo\n")
-create_report()
+
+def generate_csv(data):
+    return data
+
+
+def BuildReport(clean_build = False):
+    
+    if(clean_build == True):
+        remove("DataBase.json")
+        remove("Report.json")
+    
+    # Generate DataBase.json file which have product details and ceneo_id
+    if(path.exists("DataBase.json") == False):
+        print("Create DataBase.json")
+        create_database()
+    else:
+        print("Skipped because DataBase.json exist")
+        
+    # Generate Report.json file which have product details and data grabbed from ceneo.pl
+    if(path.exists("Report.json") == False):
+        print("Create Report.json")
+        create_report()
+    else:
+        print("Skipped because Report.json exist")
+
+    
+    # Analize data and calculate statistics
+    save_file("Report.json", analize_data(read_file("Report.json")))
+    
+    # Create CSV file
+    generate_csv(read_file("Report.json"))
+
+
+BuildReport()
